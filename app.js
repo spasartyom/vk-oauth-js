e5621168d5559832e5d660af86f8b7baaa28c9c4
Артем Спас <spasartyom@gmail.com>
2020-09-01 12:05:40 +0300

    fix creds

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(logger);
 app.set('view engine', 'pug');
 app.use(Express.static('assets'));
 
-const VKONTAKTE_APP_ID = '111111111';
-const VKONTAKTE_APP_SECRET = '1111111111111111111';
+const VKONTAKTE_APP_ID = '7582587';
+const VKONTAKTE_APP_SECRET = 'KNqKK8AIPYOr5dQvw7yn';
 
 passport.use(new VKontakteStrategy({
   clientID: VKONTAKTE_APP_ID,
